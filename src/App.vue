269f19d939873cbb8548396e7169bdf63e067450
chrislitw <chrisli.dev@gmail.com>
2023-08-09 10:48:25 +0800

    feat: babylon setup

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,11 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
+import BabylonScene from './babylon/createScene.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <BabylonScene></BabylonScene>
   </header>
 
   <RouterView />
---
+++ src/babylon/createScene.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { createScene } from './createScene.ts'
+import { ref, onMounted } from 'vue'
+
+const bjsCanvas = ref<HTMLCanvasElement | null>(null)
+
+onMounted(async () => {
+  if (bjsCanvas.value) {
+    await createScene(bjsCanvas.value)
+  }
+})
+</script>
+<template>
+  <canvas ref="bjsCanvas" width="500" height="500" />
+</template>
+<style scoped></style>
