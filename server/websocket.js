4fd54e9bd788e5642187cf3974fedbb8fa545f93
chrislitw <chrisli.dev@gmail.com>
2023-08-13 20:42:56 +0800

    feat: add websocket server

---
+++ server/websocket.js
@@ -0,0 +1,13 @@
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on("connection", function connection(ws) {
+  ws.on("error", console.error);
+
+  ws.on("message", function message(data) {
+    console.log("received: %s", data);
+  });
+
+  ws.send("something");
+});
