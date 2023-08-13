1a691ccef48e0badb73f093633d6ec25fc04f759
chrislitw <chrisli.dev@gmail.com>
2023-08-13 22:21:10 +0800

    fix: server setup

--- server/websocket.js
+++ server/websocket.js
@@ -3,6 +3,8 @@ const WebSocket = require("ws");
 const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on("connection", function connection(ws) {
+  console.log("client is connected", wss.clients.size);
+
   ws.on("error", console.error);
 
   ws.on("message", function message(data) {
