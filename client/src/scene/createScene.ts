8d8edfad37b26833174c1eee996bdc7088c625d9
chrislitw <chrisli.dev@gmail.com>
2023-08-14 23:33:52 +0800

    fix: improvement scene & websocekt env

--- client/src/scene/createScene.ts
+++ client/src/scene/createScene.ts
@@ -8,30 +8,44 @@ import {
   Color3,
   HemisphericLight
 } from '@babylonjs/core'
+
+const createBox = (scene: Scene): typeof box => {
+  const box = MeshBuilder.CreateBox('box', { size: 2 }, scene)
+  const m1 = new StandardMaterial('box-material', scene)
+  m1.diffuseColor = Color3.Green()
+  box.material = m1
+  return box
+}
+
+const createGround = (scene: Scene): typeof ground => {
+  const ground = MeshBuilder.CreateGround('ground', { width: 10, height: 10 }, scene)
+  const m2 = new StandardMaterial('ground-material', scene)
+  m2.diffuseColor = Color3.Blue()
+  ground.material = m2
+  return ground
+}
 const createScene = async (canvas: HTMLCanvasElement) => {
   // const engine = new Engine(canvas)
   const engine = new WebGPUEngine(canvas)
   await engine.initAsync()
   const scene = new Scene(engine)
 
-  const camera = new FreeCamera('camera1', new Vector3(0, 5, -10), scene)
+  const camera = new FreeCamera('camera', new Vector3(0, 5, -10), scene)
   camera.setTarget(Vector3.Zero())
   camera.attachControl(canvas, true)
 
   new HemisphericLight('light', Vector3.Up(), scene)
 
-  const box = MeshBuilder.CreateBox('box', { size: 2 }, scene)
-  const m1 = new StandardMaterial('box-material', scene)
-  m1.diffuseColor = Color3.Green()
-  box.material = m1
-
-  const plane = MeshBuilder.CreateGround('ground', { width: 10, height: 10 }, scene)
-  const m2 = new StandardMaterial('box-material', scene)
-  m2.diffuseColor = Color3.Blue()
-  plane.material = m2
+  const box1 = createBox(scene)
+  const ground1 = createGround(scene)
 
   engine.runRenderLoop(() => {
     scene.render()
+  })
+  window.addEventListener('resize', () => {
+    console.log('asdas')
+
+    engine.resize()
   })
 }
 
