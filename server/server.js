39712d8f0b5245fc0a952d5a2f7c175b033e57b4
chrislitw <chrisli.dev@gmail.com>
2023-08-14 10:36:11 +0800

    fix: deploy websocket service

--- server/server.js
+++ server/server.js
@@ -1,3 +1,27 @@
 const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const WebSocket = require("ws");
 
-require("./websocket");
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", function connection(ws) {
+  console.log("client is connected", wss.clients.size);
+  ws.send("connecting successful!");
+
+  ws.on("error", console.error);
+
+  ws.on("message", function message(data) {
+    console.log("received: ", data);
+  });
+});
+
+const PORT = process.env.PORT || 8081;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+server.listen(PORT, () => {
+  console.log(`server started at http://localhost:${PORT}`);
+});
